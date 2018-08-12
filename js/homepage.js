7c67e20bd2f97045cfe7039012827a6084621832
sm1996 <36611191+sm1996@users.noreply.github.com>
2018-08-12 14:21:38 +0100

    no more pointer-event

--- js/homepage.js
+++ js/homepage.js
@@ -1,12 +1,11 @@
 
 $(window).scroll(function() {
   $('.logoAppear').css({
-    'opacity':'0',
-    'pointer-events':'none'
+    opacity:'0'
   });
 
   $('.logo').css({
-    'opacity':'1'
+    opacity:'1'
   });
 
   var logoTop = $('.logo').offset().top;
@@ -18,37 +17,33 @@ $(window).scroll(function() {
   var currentScroll = $(window).scrollTop();
     if (currentScroll >= logoTop) {
       $('.logo').css({
-        'opacity':'0',
-        'pointer-events':'none'
+        opacity:'0',
       });
       $('.logoAppear').css({
-        'opacity':'1'
+        opacity:'1'
       });
     }
     else{
       $('.logoAppear').css({
-        'opacity':'0',
-        'pointer-events':'none'
+        opacity:'0'
       });
       $('.logo').css({
-        'opacity':'1'
+        opacity:'1'
       });
     }
 
     if (currentScroll >= logoTop && currentScroll<=triggerTop) {
 
         $('.deskNav').css({
-          'opacity':'1'
+          opacity:'1'
         });
 
     } else {
         $('.deskNav').css({
-          'opacity':'0',
-          'pointer-events':'none'
+          opacity:'0'
         });
         $('.logoAppear').css({
-          'opacity':'0',
-          'pointer-events':'none'
+          opacity:'0'
         });
     }
 });
