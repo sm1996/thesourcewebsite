e65966643bb848cedcabcdaa4a1fe744afd77982
sm1996 <shamama.muhit@googlemail.com>
2018-09-02 21:02:39 +0100

    Update product pages, css, js

---
+++ js/productpage.js
@@ -0,0 +1,256 @@
+function addSizeXS() {
+
+  $('.xs').css({
+    display:'block'
+  });
+
+  $('.xssize').css({
+    background:'#f04d44',
+    color:'#fdd80e'
+  })
+
+  $('.s').css({
+    display:'none'
+  });
+
+  $('.ssize').css({
+    background:'transparent',
+    color:'#f04d44'
+  })
+
+  $('.m').css({
+    display:'none'
+  });
+
+  $('.msize').css({
+    background:'transparent',
+    color:'#f04d44'
+  })
+
+  $('.l').css({
+    display:'none'
+  });
+
+  $('.lsize').css({
+    background:'transparent',
+    color:'#f04d44'
+  })
+
+  $('.xl').css({
+    display:'none'
+  });
+
+  $('.xlsize').css({
+    background:'transparent',
+    color:'#f04d44'
+  })
+
+}
+
+function addSizeS() {
+
+  $('.s').css({
+    display:'block'
+  });
+
+  $('.ssize').css({
+    background:'#f04d44',
+    color:'#fdd80e'
+  })
+
+  $('.xs').css({
+    display:'none'
+  });
+
+  $('.xssize').css({
+    background:'transparent',
+    color:'#f04d44'
+  })
+
+  $('.m').css({
+    display:'none'
+  });
+
+  $('.msize').css({
+    background:'transparent',
+    color:'#f04d44'
+  })
+
+  $('.l').css({
+    display:'none'
+  });
+
+  $('.lsize').css({
+    background:'transparent',
+    color:'#f04d44'
+  })
+
+  $('.xl').css({
+    display:'none'
+  });
+
+  $('.xlsize').css({
+    background:'transparent',
+    color:'#f04d44'
+  })
+
+}
+
+function addSizeM() {
+
+  $('.m').css({
+    display:'block'
+  });
+
+  $('.msize').css({
+    background:'#f04d44',
+    color:'#fdd80e'
+  })
+
+  $('.xs').css({
+    display:'none'
+  });
+
+  $('.xssize').css({
+    background:'transparent',
+    color:'#f04d44'
+  })
+
+  $('.s').css({
+    display:'none'
+  });
+
+  $('.ssize').css({
+    background:'transparent',
+    color:'#f04d44'
+  })
+
+  $('.l').css({
+    display:'none'
+  });
+
+  $('.lsize').css({
+    background:'transparent',
+    color:'#f04d44'
+  })
+
+  $('.xl').css({
+    display:'none'
+  });
+
+  $('.xlsize').css({
+    background:'transparent',
+    color:'#f04d44'
+  })
+
+}
+
+function addSizeL() {
+
+  $('.l').css({
+    display:'block'
+  });
+
+  $('.lsize').css({
+    background:'#f04d44',
+    color:'#fdd80e'
+  })
+
+  $('.xs').css({
+    display:'none'
+  });
+
+  $('.xssize').css({
+    background:'transparent',
+    color:'#f04d44'
+  })
+
+  $('.s').css({
+    display:'none'
+  });
+
+  $('.ssize').css({
+    background:'transparent',
+    color:'#f04d44'
+  })
+
+  $('.m').css({
+    display:'none'
+  });
+
+  $('.msize').css({
+    background:'transparent',
+    color:'#f04d44'
+  })
+
+  $('.xl').css({
+    display:'none'
+  });
+
+  $('.xlsize').css({
+    background:'transparent',
+    color:'#f04d44'
+  })
+
+}
+
+function addSizeXL() {
+
+  $('.xl').css({
+    display:'block'
+  });
+
+  $('.xlsize').css({
+    background:'#f04d44',
+    color:'#fdd80e'
+  })
+
+  $('.xs').css({
+    display:'none'
+  });
+
+  $('.xssize').css({
+    background:'transparent',
+    color:'#f04d44'
+  })
+
+  $('.s').css({
+    display:'none'
+  });
+
+  $('.ssize').css({
+    background:'transparent',
+    color:'#f04d44'
+  })
+
+  $('.m').css({
+    display:'none'
+  });
+
+  $('.msize').css({
+    background:'transparent',
+    color:'#f04d44'
+  })
+
+  $('.l').css({
+    display:'none'
+  });
+
+  $('.lsize').css({
+    background:'transparent',
+    color:'#f04d44'
+  })
+
+}
+
+function sizeSelect(){
+  $('.prodInfo').css({
+    display:'block'
+  });
+}
+
+function closeSize(){
+  $('.prodInfo').css({
+    display:'none'
+  });
+}
